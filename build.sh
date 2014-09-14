f3a1722ea82509d89ecbf1b0d0a16b9d24876cce
Alexandru Csete <oz9aec@gmail.com>
2014-09-14 22:07:21 +0200

    Move source files to src/

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-gcc -Wall -o ic706_server ic706_server.c common.c
-gcc -Wall -o ic706_client ic706_client.c  common.c
-gcc -Wall -o serial_gateway serial_gateway.c common.c
+gcc -Wall -o ic706_server src/ic706_server.c src/common.c
+gcc -Wall -o ic706_client src/ic706_client.c src/common.c
+gcc -Wall -o serial_gateway src/serial_gateway.c src/common.c
