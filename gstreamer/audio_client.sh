8bd2a62260a8688996a480a635262517339cf092
Alexandru Csete <oz9aec@gmail.com>
2014-10-09 05:40:14 +0000

    Don't run gstreamer pipelines in a loop.
    Systemd will take care of restarting the audio client and server.

--- gstreamer/audio_client.sh
+++ gstreamer/audio_client.sh
@@ -15,12 +15,9 @@ fi
 echo "Playing audio stream from $2:$3"
 echo "Audio device: $1"
 
-while true; do
-    gst-launch-1.0 -v -e tcpclientsrc host=$2 port=$3 ! \
-        application/x-rtp, media=audio,clock-rate=48000,payload=96 ! \
-        rtpopusdepay ! opusdec ! alsasink device=$1 sync=false
+gst-launch-1.0 -e tcpclientsrc host=$2 port=$3 ! \
+    application/x-rtp, media=audio,clock-rate=48000,payload=96 ! \
+    rtpopusdepay ! opusdec ! alsasink device=$1 sync=false
 
-    echo "Wait for 5 seconds..."
-    sleep 5
-done
+echo "Audio client exited."
 
--- gstreamer/audio_server.sh
+++ gstreamer/audio_server.sh
@@ -18,13 +18,10 @@ host=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/
 echo "Starting audio streaming server at $host:$2"
 echo "Audio device: $1"
 
-while true; do
-    gst-launch-1.0 -v alsasrc device=$1 ! \
-        audio/x-raw, format=\(string\)S16LE, channels=2, rate=48000 ! \
-        opusenc bandwidth=1101 bitrate=24000 audio=true frame-size=60 complexity=3 ! \
-        rtpopuspay ! tcpserversink host=$host port=$2
+gst-launch-1.0 -e alsasrc device=$1 ! \
+    audio/x-raw, format=\(string\)S16LE, channels=2, rate=48000 ! \
+    opusenc bandwidth=1101 bitrate=24000 audio=true frame-size=60 complexity=3 ! \
+    rtpopuspay ! tcpserversink host=$host port=$2
 
-    echo "Sleeping for 5 seconds..."
-    sleep 5
-done
+echo "Audio server exited"
 
