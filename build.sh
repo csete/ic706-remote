3b84740ab2f3b0af424a90b6f425dde24e996490
Alexandru Csete <oz9aec@gmail.com>
2014-10-09 01:31:48 +0000

    Only build applications that we use.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-gcc -Wall -o audio_client src/audio_client.c src/common.c
+# gcc -Wall -o audio_client src/audio_client.c src/common.c
 
-gcc -Wall -o audio_server src/audio_server.c src/audio_util.c src/common.c \
-            `pkg-config --cflags --libs alsa`
+# gcc -Wall -o audio_server src/audio_server.c src/audio_util.c src/common.c \
+#            `pkg-config --cflags --libs alsa`
 
 gcc -Wall -o ic706_server src/ic706_server.c src/common.c
 gcc -Wall -o ic706_client src/ic706_client.c src/common.c
 
-gcc -Wall -o serial_gateway src/serial_gateway.c src/common.c
+# gcc -Wall -o serial_gateway src/serial_gateway.c src/common.c
