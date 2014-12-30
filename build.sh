51737099a20b896b73395e9afd22cdf8c34a71df
Alexandru Csete <oz9aec@gmail.com>
2014-12-31 00:05:10 +0100

    Add makefile to replace the build.sh script.

--- build.sh
+++
@@ -1,9 +0,0 @@
-gcc -Wall -o ic706_server src/ic706_server.c src/common.c
-gcc -Wall -o ic706_client src/ic706_client.c src/common.c
-# gcc -Wall -o serial_gateway src/serial_gateway.c src/common.c
-
-## These do not yet work
-# gcc -Wall -o audio_client src/audio_client.c src/common.c
-
-# gcc -Wall -o audio_server src/audio_server.c src/audio_util.c src/common.c \
-#            `pkg-config --cflags --libs alsa`
